f5e4b2c54c55494645418ec10b7a543ac99b3f05
Nathaniel Laptop <nathanielyu917@gmail.com>
2020-12-12 22:46:03 -0800

    installed depedencies, created schema and seed

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE `burgers` (
+  id INT( 11 ) AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR( 255 ) NOT NULL,
+  devoured BOOLEAN NOT NULL,
+
+  PRIMARY KEY ( id ) 
+);
---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+/* Seeds for burgers_db */
+USE burgers_db;
+
+/* seeds */
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Whopper", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Big Mac", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Western Bacon Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double double", false);
